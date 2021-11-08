25136b1d1a53eddc89eaa0eca7b890eef94065e7
Matthew Khoo <matthew.khoo.mk@gmail.com>
2021-11-08 23:55:23 +0800

    Modified subplot method

--- main/viz_utils.py
+++ main/viz_utils.py
@@ -7,16 +7,18 @@ sns.color_palette("viridis", as_cmap=True)
 
 """Plotting utility for EDA"""
 
-def cat_subplots(data: list, subrow: int)->None:
+def plot_subplots(data: list, subrow: int, figsize_ep = 5)->None:
     subcol = ceil(len(data)/subrow)
-    fig, axes = plt.subplots(subrow, subcol, figsize = (subrow*8, subcol*8))
-    plt.tight_layout()
+    fig, axes = plt.subplots(subrow, subcol, figsize = (subrow*figsize_ep, subcol*figsize_ep))
+    
     current = 0
     for subaxes in axes:
         for axis in subaxes:
             data[current].plot(ax = axis, kind='bar')
             current += 1
-            if current == len(data): return
+            if current == len(data): 
+                plt.tight_layout()
+                return
 
 def corr_heatmap(data: DataFrame)->None:
     plt.figure(figsize=(15, 10))
